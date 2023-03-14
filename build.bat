cd404fb50edcdab057c1193b2694cdefae231d31
Dekita <dekitarpg@gmail.com>
2023-03-14 18:36:45 +0000

    Update build.bat
    Now allows for an array of mods to be copied

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+::silencio 
+@echo off
+
 :: ================== ::
 :: Editable Variables ::
 :: ================== ::
@@ -18,19 +21,67 @@ set output_path=D:\UE_PhoenixOutput
 :: game_path=the path where your game is installed
 set game_path=D:\SteamLibrary\steamapps\common\Hogwarts Legacy
 
-:: pak_chunk_id=the pak chunk to copy into your game directory
-set pak_chunk_id=111
+:: launch_game_after_copy=set to "true" to auto launch game
+set launch_game_after_copy="true"
 
-:: new_pak_name=the name to give the copied .pak, .ucas, and .utoc
-set new_pak_name=Dek%pak_chunk_id%_TEST_P
+:: name_prefix=the name to prefix along with chunk id onto pak/ucas/utoc
+set name_prefix=Dek
+
+:: mod_data[]=an array of {chunk_id, name} for each mod to copy over
+set mod_data[0].chunk_id=100
+set mod_data[0].name=MyAwesomeModOne
+
+set mod_data[1].chunk_id=101
+set mod_data[1].name=MyAwesomeModTwo
+
+set mod_data[2].chunk_id=102
+set mod_data[2].name=MyAwesomeModThree
+
 
 :: ================= ::
 :: DO NOT EDIT BELOW ::
 :: ================= ::
-set REAL_OUTPATH=%output_path%\WindowsNoEditor\phoenix\Content\Paks\pakchunk%pak_chunk_id%-WindowsNoEditor
-set MODS_OUTPATH=%game_path%\Phoenix\Content\Paks\~mods\%new_pak_name%
+
+:: rebuild uproject files
 call %engine_path%\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -nocompileeditor -nop4 -project=%project_path%/phoenix.uproject -cook -stage -archive -archivedirectory=%output_path% -package -ddc=DerivedDataBackendGraph -iostore -pak -iostore -prereqs -manifests -targetplatform=Win64 -target=phoenix -clientconfig=Shipping -utf8output -compile
-copy /Y "%REAL_OUTPATH%.pak" "%MODS_OUTPATH%.pak"
-copy /Y "%REAL_OUTPATH%.ucas" "%MODS_OUTPATH%.ucas"
-copy /Y "%REAL_OUTPATH%.utoc" "%MODS_OUTPATH%.utoc"
-"%game_path%\Phoenix\Binaries\Win64\HogwartsLegacy.exe"
+
+:: simple loop to get length of mods_data array
+set length=0
+
+:Loop 
+:: check if array element exists and increase length
+if defined mod_data[%length%].name ( 
+    set /a length+=1
+    GOTO :Loop 
+)
+:: reduce length by for loop
+set /a length-=1
+
+
+:: allow local variables to be set within loops
+SetLocal EnableDelayedExpansion
+
+:: loop to iterate over each defined mod_data
+for /l %%x in (0, 1, %length%) do (
+    :: setting various variables
+    call set name=!mod_data[%%x].name!
+    call set chunk=!mod_data[%%x].chunk_id!
+    call set temp_outpath=%output_path%\WindowsNoEditor\phoenix\Content\Paks\pakchunk!chunk!-WindowsNoEditor
+    call set temp_modpath=%game_path%\Phoenix\Content\Paks\~mods\%name_prefix%!chunk!_!name!_P
+    
+    :: echo for information
+    echo Copying %name_prefix%!chunk!_!name!_P.pak/.ucas/.utoc
+    echo from: !temp_outpath!
+    echo to: !temp_modpath!
+    
+    :: actually copy files
+    copy /Y "!temp_outpath!.pak" "!temp_modpath!.pak" 
+    copy /Y "!temp_outpath!.ucas" "!temp_modpath!.ucas" 
+    copy /Y "!temp_outpath!.utoc" "!temp_modpath!.utoc" 
+)
+
+@echo Finished Copying files, now launching game...
+
+if %launch_game_after_copy% == "true" (
+    "%game_path%\Phoenix\Binaries\Win64\HogwartsLegacy.exe"
+)
